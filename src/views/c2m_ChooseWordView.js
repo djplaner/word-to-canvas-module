6d881fd35ec1d096b568b13bbfc0c3f583e76029
David Jones <davidthomjones@gmail.com>
2022-02-13 15:10:14 +1000

    Early interface transition

--- src/views/c2m_ChooseWordView.js
+++ src/views/c2m_ChooseWordView.js
@@ -3,8 +3,20 @@
 import { c2m_View } from './c2m_View.js';
 
 const CHOOSE_WORD_HTML = `
-<div class="border border-trbl pad-box">
-<h3>Create new module from Word document</h3>
+
+<div class="border border-trbl">
+<h4>Create new module from Word document</h4>
+<div class="w2c-nav">
+  <ul>
+    <li class="w2c-nav w2c-active"><a href="#">1. Choose .docx</a></li>
+	<li class="w2c-nav"><a href="#">2. Check HTML</a> </li>
+	<li class="w2c-nav"><a href="#">3. Check Module</a> </li>
+	<li class="w2c-nav"><a href="#">4. Complete?</a></li>
+    <li class="w2c-nav w2c-close"> <a class="icon-x"></a> </li>
+  </ul>
+</div>
+
+<div class="w2c-content pad-box-mini">
 
 <p color="secondary">Step 1 of 4: Provide Word document</p>
 
@@ -21,8 +33,58 @@ const CHOOSE_WORD_HTML = `
 <p><em>Some link to documentation</em></p>
 <p><em>Some method to cancel operation</em></p>
 <p><em>Visual indication of process - e.g. tabs</em></p>
+</div>
 
 </div>
+
+<style>
+
+.w2c-content {
+    clear:both;
+}
+
+.w2c-nav ul  {
+	list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden ;
+    background-color: #eee; 
+    width:100%;
+}
+
+li.w2c-active {
+    background-color: #aaa;
+    font-weight: bold;
+}
+
+li.w2c-close {
+    float: right !important;
+    border-right: none !important;
+}
+
+.w2c-nav ul li {
+    float:left;
+    border-right: 1px solid #000;
+}
+
+li.w2c-nav a {
+    display: block;
+    padding: 0.5em;
+    text-align: center;
+    text-decoration: none;
+    color: black;
+}
+
+.w2c-nav li a:hover {
+    background-color: #111;
+}
+
+.w2c-nav li:nth-child(4) {
+    border-right: none;
+}
+
+
+</style>
 
 `;
 
