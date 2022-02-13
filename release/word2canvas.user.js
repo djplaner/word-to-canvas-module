43c1064c305f1baff88f49b9f45c7e8f348dd3a0
David Jones <davidthomjones@gmail.com>
2022-02-13 16:32:25 +1000

    And another fix?

--- release/word2canvas.user.js
+++ release/word2canvas.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Word 2 Canvas Module
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Userscript to create a new Canvas LMS Module from a Word document
 // @author       David Jones
-// @match        *://*/courses/.*/modules.*
+// @match        https://*/courses/*/modules*
 // @grant        none
 // @source       https://github.com/djplaner/word-to-canvas-module.git
 // @license      MIT
