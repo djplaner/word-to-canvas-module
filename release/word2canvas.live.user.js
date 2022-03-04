914bb30267c58df86f1939eded9c6bae33c7d579
David Jones <davidthomjones@gmail.com>
2022-03-04 12:45:20 +1000

    Bump version # on live

--- release/word2canvas.live.user.js
+++ release/word2canvas.live.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Word 2 Canvas Module
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.4.2
 // @description  Userscript to create a new Canvas LMS Module from a Word document
 // @author       David Jones
 // @match        https://*/courses/*/modules*
