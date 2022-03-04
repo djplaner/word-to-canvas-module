6ae636f046341420354b5f79b9f488cc5e7293f7
David Jones <davidthomjones@gmail.com>
2022-03-04 12:43:34 +1000

    Use the CDN "better"

---
+++ release/word2canvas.live.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Word 2 Canvas Module
+// @namespace    http://tampermonkey.net/
+// @version      1.1.0
+// @description  Userscript to create a new Canvas LMS Module from a Word document
+// @author       David Jones
+// @match        https://*/courses/*/modules*
+// @grant        none
+// @source       https://github.com/djplaner/word-to-canvas-module.git
+// @license      MIT
+// @homepage     https://github.com/djplaner/word-to-canvas-module
+// @require      https://cdnjs.cloudflare.com/ajax/libs/mammoth/0.3.10/mammoth.browser.min.js
+// @require      https://cdn.jsdelivr.net/gh/djplaner/word-to-canvas-module@1.4.2/release/word2canvas.min.js
+// ==/UserScript==
