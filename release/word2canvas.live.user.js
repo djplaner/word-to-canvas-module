ba3c82b4e51991ad7e459f51c52840364fa2bb65
David Jones <davidthomjones@gmail.com>
2022-03-14 06:59:14 +1000

    Better live CDN?

--- release/word2canvas.live.user.js
+++ release/word2canvas.live.user.js
@@ -10,5 +10,5 @@
 // @license      MIT
 // @homepage     https://github.com/djplaner/word-to-canvas-module
 // @require      https://cdnjs.cloudflare.com/ajax/libs/mammoth/0.3.10/mammoth.browser.min.js
-// @require      https://cdn.jsdelivr.net/gh/djplaner/word-to-canvas-module@1.4.2/release/word2canvas.min.js
+// @require      https://raw.githack.com/djplaner/word-to-canvas-module/main/release/word2canvas.min.js
 // ==/UserScript==
