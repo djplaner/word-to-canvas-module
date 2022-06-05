ddae25be252516c228063e83974004764cc31057
David Jones <davidthomjones@gmail.com>
2022-06-06 07:22:07 +1000

    fix path for dev local file userscript

--- dist/devWord2Canvas.user.js
+++ dist/devWord2Canvas.user.js
@@ -10,7 +10,7 @@
 // @license      MIT
 // @homepage     https://github.com/djplaner/word-to-canvas-module
 // @require      https://cdnjs.cloudflare.com/ajax/libs/mammoth/0.3.10/mammoth.browser.min.js
-// @require      file:///C:\Users\s2986288\code\word-to-canvas-module\dist\word2canvas.user.js
+// @require      file:///E:\code\word-to-canvas-module\dist\word2canvas.user.js
 // @run-at       document-end
 // ==/UserScript==
 
