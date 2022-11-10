35265f531dbdf2eecac4f9438e3557d916702484
David Jones <davidthomjones@gmail.com>
2022-11-11 08:10:10 +1000

    Auto update maybe

--- release/word2canvas.user.js
+++ release/word2canvas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Word 2 Canvas Module
 // @namespace    http://tampermonkey.net/
-// @version      2.0.13
+// @version      2.0.14
 // @description  Userscript to create a new Canvas LMS Module from a Word document
 // @author       David Jones
 // @match        https://*/courses/*
@@ -9,7 +9,7 @@
 // @source       https://github.com/djplaner/word-to-canvas-module.git
 // @license      MIT
 // @homepage     https://github.com/djplaner/word-to-canvas-module
-// @updateURL    https://github.com/djplaner/word-to-canvas-module/raw/main/dist/word2canvas.user.js
+// @updateURL    https://github.com/djplaner/word-to-canvas-module/raw/main/release/word2canvas.user.js
 // @supportURL   https://github.com/djplaner/word-to-canvas-module/issues
 // @require      https://cdnjs.cloudflare.com/ajax/libs/mammoth/0.3.10/mammoth.browser.min.js
 // @require      https://rawcdn.githack.com/djplaner/canvas-collections/62a4248058d13d32c574f0b620760891651587a7/src/juice/juice_client.js
