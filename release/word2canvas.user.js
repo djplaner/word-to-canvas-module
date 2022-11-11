f221cdbeb6bb2ebcfa409a84877976388affbf4a
David Jones <davidthomjones@gmail.com>
2022-11-11 11:34:59 +1000

    Update to recent version of Mammoth

--- release/word2canvas.user.js
+++ release/word2canvas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Word 2 Canvas Module
 // @namespace    http://tampermonkey.net/
-// @version      2.0.16
+// @version      2.0.17
 // @description  Userscript to create a new Canvas LMS Module from a Word document
 // @author       David Jones
 // @match        https://*/courses/*
@@ -12,7 +12,7 @@
 // @updateURL    https://github.com/djplaner/word-to-canvas-module/raw/main/release/word2canvas.user.js
 // @downloadURL  https://github.com/djplaner/word-to-canvas-module/raw/main/release/word2canvas.user.js
 // @supportURL   https://github.com/djplaner/word-to-canvas-module/issues
-// @require      https://cdnjs.cloudflare.com/ajax/libs/mammoth/0.3.10/mammoth.browser.min.js
+// @require      https://cdn.jsdelivr.net/npm/mammoth@1.5.1/mammoth.browser.min.js
 // @require      https://rawcdn.githack.com/djplaner/canvas-collections/62a4248058d13d32c574f0b620760891651587a7/src/juice/juice_client.js
 // ==/UserScript==
 
@@ -39,7 +39,7 @@ class c2m_View {
 		this.model = model;
 		this.controller = controller;
 
-		this.version = "2.0.16";
+		this.version = "2.0.17";
 	}
 
 
